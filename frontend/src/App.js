0925dc0ab8f78fb9eb5dda167eb60f38837ca295
John Vea <John Vea>
2024-07-31 11:11:40 -0400

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+
+}
+
+export default App;
